da116ec614abba1cad94acc7ecbf48472c3fb509
Noah Lanson <lanson.noah@gmail.com>
2021-09-25 14:44:33 +1000

    initial commit

---
+++ examples/src/main.rs
@@ -0,0 +1,22 @@
+use btc_tx::*;
+
+fn main() {
+    // let inputs = vec![
+    //     btc_tx::tx::Input { },
+    //     btc_tx::tx::Input { }
+    // ];
+
+    // let outputs = vec![
+    //     btc_tx::tx::Output { },
+    //     btc_tx::tx::Output { }
+    // ];
+    
+    // let tx1 = btc_tx::tx::Tx::create(
+    //     [0x01, 0x00, 0x00, 0x00], 
+    //     inputs,
+    //     outputs,
+    //     [0xff, 0xff, 0xff, 0xff]
+    // );
+
+    // println!("{:?}", tx1.input_count);
+}
