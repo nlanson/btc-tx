5bdc89abb0d8a83ac7feec8e3ff95422db3e0203
Noah Lanson <lanson.noah@gmail.com>
2021-11-05 18:57:06 +1100

    use prelude module

--- examples/src/main.rs
+++ examples/src/main.rs
@@ -1,22 +1,9 @@
 /*
     The examples with broadcasted TXs can be used in unit tests
 */
-use btc_tx::{
-    util::{
-        serialize::Serialize,
-        Network
-    },
-    tx::{
-        SigningData,
-        SigHash,
-        TxBuilder,
-        Tx,
-        Script
-    }
-};
-
+use btc_tx::prelude::*;
+use btc_tx::prelude::Serialize;
 use btc_keyaddress::prelude::*;
-use btc_keyaddress::key::PrivKey as PrivKey;
 
 fn main() {
    //See unit tests under ../src/tx/builder/txbuilder.rs for examples
